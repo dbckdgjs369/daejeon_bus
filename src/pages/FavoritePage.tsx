5d48a85b672c718fae986e2f77260e94c5cdabfd
Yoo Chang Heon <dbckdgjs1130@naver.com>
2022-11-24 13:18:52 +0900

    feat: 즐겨찾기 페이지 layout 구현

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -1,5 +1,14 @@
+import FavoriteItem from "../components/Item/FavoriteItem";
+
 const FavoritePage = () => {
-  return <div>FavoritePage</div>;
+  return (
+    <div>
+      <FavoriteItem content="정류장" isTitle />
+      <FavoriteItem content="둥지아파트" isTitle={false} />
+      <FavoriteItem content="둥지아파트" isTitle={false} />
+      <FavoriteItem content="노선" isTitle />
+    </div>
+  );
 };
 
 export default FavoritePage;
