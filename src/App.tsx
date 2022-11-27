35e9a4467b71d77d1fb99ed897773e1e661bd638
Yoo Chang Heon <dbckdgjs1130@naver.com>
2022-11-27 13:53:00 +0900

    feat: 페이지 라우팅 설정 및 default 페이지 favorite으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import MainPage from "./pages/MainPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Test from "./pages/Test";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import RoutePage from "./pages/RoutePage";
+import StationPage from "./pages/StationPage";
+import SubwayPage from "./pages/SubwayPage";
+import MainHeader from "./components/Header/MainHeader";
+import Nav from "./components/Nav/Nav";
+import { SelectedProps } from "./components/Nav/Nav";
+import FavoritePage from "./pages/FavoritePage";
 
 const App = () => {
+  const [selectedPage, setSelectedPage] = useState<SelectedProps>("favorite");
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/a" element={<Test />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <MainHeader />
+      <BrowserRouter>
+        <Nav setSelectedPage={setSelectedPage} />
+        <Routes>
+          <Route path="/" element={<Navigate to={"/favorite"} />} />
+          <Route path="/favorite" element={<FavoritePage />} />
+          <Route path="/route:routeNumber" element={<RoutePage />} />
+          <Route path="/station" element={<StationPage />} />
+          <Route path="/subway" element={<SubwayPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
