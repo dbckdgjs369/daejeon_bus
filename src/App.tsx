935fc79aa69a65cf6fbf4623c25056b76e14a24c
Yoo Chang Heon <dbckdgjs1130@naver.com>
2022-12-01 16:04:52 +0900

    feat: 페이지마다 헤더랑 nav 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import RoutePage from "./pages/RoutePage";
 import StationPage from "./pages/StationPage";
 import SubwayPage from "./pages/SubwayPage";
-import MainHeader from "./components/Header/MainHeader";
-import Nav from "./components/Nav/Nav";
-import { SelectedProps } from "./components/Nav/Nav";
 import FavoritePage from "./pages/FavoritePage";
 import BusRoutePage from "./pages/BusRoutePage";
 
 const App = () => {
-  const [selectedPage, setSelectedPage] = useState<SelectedProps>("favorite");
   return (
     <>
-      <MainHeader />
       <BrowserRouter>
-        <Nav setSelectedPage={setSelectedPage} />
-        <Routes>
-          <Route path="/" element={<Navigate to={"/favorite"} />} />
-          <Route path="/favorite" element={<FavoritePage />} />
-          <Route path="/allroute:" element={<RoutePage />} />
-          <Route path="/route/:busNumber" element={<BusRoutePage />} />
-          <Route path="/station" element={<StationPage />} />
-          <Route path="/subway" element={<SubwayPage />} />
-        </Routes>
+        <>
+          <Routes>
+            <Route path="/" element={<Navigate to={"/favorite"} />} />
+            <Route path="/favorite" element={<FavoritePage />} />
+            <Route path="/allroute" element={<RoutePage />} />
+            <Route path="/route/:busNumber" element={<BusRoutePage />} />
+            <Route path="/station" element={<StationPage />} />
+            <Route path="/subway" element={<SubwayPage />} />
+          </Routes>
+        </>
       </BrowserRouter>
     </>
   );
--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -1,8 +1,14 @@
+import Nav, { SelectedProps } from "../components/Nav/Nav";
 import FavoriteItem from "../components/Item/FavoriteItem";
+import { useState } from "react";
+import Header from "../components/Header/MainHeader";
 
 const FavoritePage = () => {
+  const [selectedPage, setSelectedPage] = useState<SelectedProps>("favorite");
   return (
     <div>
+      <Header />
+      <Nav />
       <FavoriteItem content="정류장" isTitle />
       <FavoriteItem content="둥지아파트" isTitle={false} />
       <FavoriteItem content="둥지아파트" isTitle={false} />
