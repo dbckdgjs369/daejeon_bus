97379adaabe91d1bd76bf77cc2830a9adab93682
Yoo Chang Heon <dbckdgjs1130@naver.com>
2022-12-06 16:23:40 +0900

    refactor: 아이템이 아닌 경우 눌렀을 때 경로 변경 제거

--- src/components/Item/FavoriteItem.tsx
+++ src/components/Item/FavoriteItem.tsx
@@ -13,19 +13,46 @@ const Wrapper = styled(Link)<WrapperProps>`
   font-weight: ${(props) => (props.isTitle ? "800" : "500")};
   border-left: ${(props) => (props.isTitle ? "8px solid lightgreen" : "none")};
 `;
+const Title = styled.div<WrapperProps>`
+  display: flex;
+  justify-content: flex-start;
+  color: black;
+  text-decoration: none;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+  height: 2.5rem;
+  padding: ${(props) => (props.isTitle ? "0 0.5rem" : "0 calc(0.5rem + 8px)")};
+  font-weight: ${(props) => (props.isTitle ? "800" : "500")};
+  border-left: ${(props) => (props.isTitle ? "8px solid lightgreen" : "none")};
+`;
 
 interface WrapperProps {
   isTitle: boolean;
 }
 interface ItemProps extends WrapperProps, React.HTMLAttributes<HTMLElement> {
   content: string;
+  itemType?: "station" | "busNumber";
 }
 
-const Item = ({ content, isTitle, ...rest }: ItemProps) => {
+const Item = ({ content, isTitle, itemType, ...rest }: ItemProps) => {
   return (
-    <Wrapper to={`/route/${content}`} isTitle={isTitle} {...rest}>
-      {content}
-    </Wrapper>
+    <>
+      {isTitle ? (
+        <Title isTitle={isTitle}>{content}</Title>
+      ) : (
+        <>
+          {itemType === "busNumber" ? (
+            <Wrapper to={`/route/${content}`} isTitle={isTitle} {...rest}>
+              {content}
+            </Wrapper>
+          ) : (
+            <Wrapper to={`/station/${content}`} isTitle={isTitle} {...rest}>
+              {content}
+            </Wrapper>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
